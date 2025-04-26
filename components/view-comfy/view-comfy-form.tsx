35c7249669ace70d387885508e695616a603c360
伶歡 <14261902+zzy17539555805@user.noreply.gitee.com>
2025-04-26 21:16:25 +0800

    调整配置文件

--- components/view-comfy/view-comfy-form.tsx
+++ components/view-comfy/view-comfy-form.tsx
@@ -192,7 +192,7 @@ export function ViewComfyForm(args: {
                                         // @ts-ignore
                                         if (field.inputs.length > 0) {
                                             // 检查是否是文本编码器或上传图片组件
-                                            const isSpecialComponent = field.title === "CLIP文本编码" || field.title === "加载图像";
+                                            const isSpecialComponent = field.title === "CLIP文本编码" || field.title === "加载图像" || field.title === "CLIP文本编码器";
                                             
                                             if (isSpecialComponent) {
                                                 // 特殊组件不显示圆角方框
